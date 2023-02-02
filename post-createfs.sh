687bb4963d4914413c12d51ad7f54512c4e95e33
Bernhard Petschina <bernhard.petschina@gmail.com>
2023-02-02 16:09:25 +0100

    updated uboot-env to read the kernel from squashfs instead of the boot partition
    the boot partition is now empty and used as a dummy

--- post-createfs.sh
+++ post-createfs.sh
@@ -13,12 +13,3 @@ if grep -iq "Some images are invalid" $NERVES_DEFCONFIG_DIR/build.log; then
   echo "Uboot created an invalid image. Is the path to BL31 correct?"
   exit 1
 fi
-
-# Copy all dtb files to the respective output folder (fwup will ignore most of these and only include the ones specified in fwup.conf in the final image)
-linuxDir=`find $BINARIES_DIR/../build -name 'vmlinux' -type f | xargs dirname`
-cp -a $NERVES_DEFCONFIG_DIR/uboot/vars.txt $BINARIES_DIR/
-mkdir -p $BINARIES_DIR/rockchip/overlays
-if [ -d ${linuxDir}/arch/arm64/boot/dts/rockchip/overlay ]; then
-  cp -a ${linuxDir}/arch/arm64/boot/dts/rockchip/overlay/*.dtbo $BINARIES_DIR/rockchip/overlays
-fi
-cp -a $BINARIES_DIR/*.dtb $BINARIES_DIR/rockchip
